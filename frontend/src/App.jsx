47e7c79b9b5a4d8c48abb7dc70ffb09460fa0855
Saurabh Singh <saurabh5532u@gmail.com>
2025-08-15 21:13:13 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,8 @@ import Followers from "./pages/Followers/Followers";
 import Notification from "./pages/Notifications/Noticfication";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPAssword";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword.jsx";
+
 import ProfileEdit from "./pages/profileEdit/profileEdit";
 import UserProfile from "./pages/userProfile/UserProfile";
 import Story from "./pages/Story/Story";

33ee42c3db09461788882eb6b34e98b8ebcdc3e1
saurabh24thakur <saurabh5532u@gmail.com>
2025-11-29 09:45:48 +0530

    fixes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB= async ()=>{
-   try {
+  try {
     await mongoose.connect(process.env.MONGO_URL)
     console.log("MongoDB connect successfully")
-   } catch (error) {
+  } catch (error) {
     console.log("Error during mongo connection ",error)
-   }
+  }
 };
 
 export default connectDB;

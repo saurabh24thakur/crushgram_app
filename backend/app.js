ee8a87bd97ce99c203439cc32673df6ff2fab7cd
saurabh24thakur <saurabh5532u@gmail.com>
2025-11-29 10:24:31 +0530

    fixes: socket.io

--- backend/app.js
+++ backend/app.js
@@ -14,34 +14,25 @@ import storyRouter from './routes/story.routes.js';
 import messageRouter from './routes/message.routes.js';
 import { app, server } from './socket.js';
 
-
-
-
-
-
-
-
-
 const port=process.env.PORT;
-
 
 app.use(express.json());     
 app.use(cookieParser());     
 
-// Middleware to ensure DB is connected before handling requests
 app.use(async (req, res, next) => {
   await connectDB();
   next();
 });
 
 app.use(cors({
-    origin: "https://www.procoder.dpdns.org", 
+    origin: [
+      "http://localhost:5173",
+      "https://api.procoder.dpdns.org",
+    ],
     credentials: true 
   }));
   app.use(express.static("public"));
 
-
-  
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
 app.use("/api/post",postRouter);
@@ -49,16 +40,18 @@ app.use("/api/vibez",vibezRouter);
 app.use("/api/story",storyRouter);
 app.use("/api/message",messageRouter);
 
-
 app.use("/temp", express.static(path.resolve("temp")));
-
-
 
 app.get('/',(req,res)=>{
     res.json({message:"app is working"});
 })
 
-server.listen(port,()=>{
-    connectDB()
-    console.log("BAckend is running at:",port);
-})
+connectDB();
+
+if (!process.env.VERCEL) {
+    server.listen(port, () => {
+        console.log("Backend is running at:", port);
+    });
+}
+
+export default app;
